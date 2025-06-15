feeb2127fdbc4372db502fafaf2e1d10a2186fc4
tdrose01 <rosebud0585@gmail.com>
2025-06-15 18:03:42 -0400

    Add lint step and navigation note

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <header class="p-4">
+      <button
+        @click="goBack"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Back
+      </button>
+    </header>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goBack() {
+  router.back()
 }
 </script>
 
