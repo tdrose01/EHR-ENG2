3918fc90c2381c95a2e2090279e2c3ab70e4d6f3
tdrose01 <rosebud0585@gmail.com>
2025-06-15 15:37:19 -0400

    update scripts

--- db/schema.sql
+++ db/schema.sql
@@ -22,12 +22,16 @@ CREATE TABLE login_audit (
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
-
 -- Example admin user for testing
 INSERT INTO users (email, password_hash)
 VALUES (
   'admin@example.com',
   '$2b$10$jeY.en9U2c2J9gAQYZAas.yEGkAwwQ.GTsQqiQ5VLzyUASDs5MmRW'
 );
-=======
+
+-- Grant necessary permissions to the web user
+CREATE USER web WITH PASSWORD 'webpass';
+GRANT ALL PRIVILEGES ON DATABASE "ehr-eng2" TO web;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO web;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO web;
 
